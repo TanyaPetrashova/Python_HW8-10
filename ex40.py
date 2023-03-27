f3b49bbab8bbdb82c3609750793bb6ca8e1dcda5
tanya <zharik08@mail.ru>
2023-03-27 14:33:38 +0800

    Определение максимального значения Households

--- ex40.py
+++ ex40.py
@@ -8,8 +8,19 @@ df = pd.read_csv('sample_data/california_housing_train.csv')
 
 filtered_df = df[(df['population'] >= 0) & (df['population'] <= 500)]
 
-print(filtered_df.head())
+print(filtered_df.head(10))
 
 mean_house_valie = filtered_df['median_house_value'].mean()
 
-print(mean_house_valie)
+print('Средняя стоимость дома, где количество людей от 0 до 500 --> ', mean_house_valie)
+
+""" Задача 42:
+Узнать какая максимальная households в зоне минимального значения population. """
+
+min_population = df['population'].min()
+
+filtered_df2 = df[df['population'] == min_population]
+
+max_households = filtered_df2['households'].max()
+
+print("Максимальное значение households в зоне с минимальным значением population: ", max_households)
